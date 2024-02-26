81a4cc7302bb387211c5c7e354824f5f94bc0773
MDkamrulJaman <mdkamruljamanjk@gmail.com>
2024-02-26 19:56:25 +0100

    practicing python advance

---
+++ main.py
@@ -0,0 +1,43 @@
+print("The program is running")
+
+# Variables
+
+x= str(10)
+y= int(1.5)
+z= float(1)
+j="jaman"
+
+# Print Variables 
+
+print(x,y,z,j)
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(j))
+
+
+
+
+
+# Camel Case
+myVariableName = "Md"
+
+# Pascal Case
+MyVariableName = "Kamrul"
+
+# Snake Case
+my_variable_name = "Jaman"
+
+print(myVariableName,MyVariableName,my_variable_name)
+
+
+# User input
+
+name = input("Enter the name:")
+print("The name is:" + name + "\n")
+age = input("Enter the age:")
+print("The age is:",age + "\n")
+
+
+print("The name is:" + name + "\n" + "The age is:", float(age) )
