72f9642bbfcd03a2aa5a438077f53226772d0b6b
MDkamrulJaman <mdkamruljamanjk@gmail.com>
2024-02-27 21:33:22 +0100

    data-types

---
+++ data_types.py
@@ -0,0 +1,90 @@
+
+r= range(10)
+
+print(type(r))
+print(r)
+
+
+# str
+j = "all data-types"
+print(type(j))
+
+# int
+a = 20
+print(type(a))
+
+
+# float
+m= 20.5
+print(type(m))
+
+
+# complex
+n= 2 + 1j
+print(type(n))
+print(n)
+
+
+
+# list
+b = ["Md", "kamrul", "jaman"]
+print(type(b))
+print(b)
+
+
+
+# tuple
+c= ("r","k","j")
+print(type(c))
+print(c)
+
+
+
+# set
+d = {"r", "k","j"}
+print(type(d))
+print(d)
+
+# dict
+e = {"name":"jaman", "age": 23}
+print(e)
+print(type(e))
+
+# frozenset
+f = frozenset({"r","k","j"})
+print(f)
+print(type(f))
+
+# boolean
+x = True
+y = False
+print(x)
+print(type(y))
+
+# nonetype
+
+z = None
+print(type(z))
+
+
+# bytes
+
+k = b"jaman"
+print(k)
+print(type(k))
+
+
+l = bytearray(5)
+print(l)
+print(type(l))
+
+m = memoryview(bytes(5))
+print(m)
+print(type(m))
+
+
+
+
+
+
+
