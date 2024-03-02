307159b55b38ba9800a86280bdcffdb8fb1d9577
MDkamrulJaman <mdkamruljamanjk@gmail.com>
2024-03-02 22:08:15 +0100

    array practice

--- Array/arrays.py
+++ Array/arrays.py
@@ -1,6 +1,6 @@
 
 myArray = ["R","K","J","M","B"]
-
+myNewArray={"p","o","Q"}
 # access array
 for x in myArray:
     print(x)
@@ -21,3 +21,32 @@ print(myArray) #['R', 'K', 'J', 'M', 'L']
 myArray.pop(4)  # with index number
 
 print(myArray) #['R', 'K', 'J', 'M']
+
+
+
+myArray.reverse()
+
+print(myArray)  #['B', 'M', 'J', 'K', 'R']
+
+
+
+myArray.sort(reverse=True)
+
+print(myArray) #['R', 'M', 'K', 'J', 'B']
+
+
+
+def myfunc(take):
+  return take["age"]
+
+
+names = [{"name":"R", "age":"2022"},{"name":"Md", "age":"2023"},{"name":"K","age":"202024"}]
+
+names.sort(key=myfunc, reverse=True)
+print(names)
+
+
+
+myArray.extend(myNewArray)
+
+print(myArray) #['R', 'K', 'J', 'M', 'B', 'p', 'o', 'Q']
